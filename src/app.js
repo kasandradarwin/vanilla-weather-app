c6b823f9d5e3e9dba71fc6651b7f7714e9b7b563
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2021-10-11 08:54:54 -0700

    minor stying adjustments
    Added a horizontal line above the forecast

--- src/app.js
+++ src/app.js
@@ -105,7 +105,8 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png
+    `
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
